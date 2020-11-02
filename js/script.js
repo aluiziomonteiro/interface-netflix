34a70602cb36f94b7e7807f661fe1891a8a2599a
junior <insejunior@gmail.com>
2020-11-02 04:53:21 -0300

    Interface

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function assistir(){
+    window.open("https://www.netflix.com/br/title/70178217");
+}
+
+function info(){
+    window.open("https://www.themoviedb.org/tv/1425-house-of-cards?language=pt-BR");
+}
